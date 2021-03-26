da3d59d5db83003c2127bdf4c512e948711a7041
UtopiaXC <dys1025@sina.com>
2021-03-26 11:41:57 +0800

    增加引导

--- bot_plugins/help.py
+++ bot_plugins/help.py
@@ -1,6 +1,7 @@
 from nonebot.command import CommandSession
 from nonebot.experimental.plugin import on_command
 import time
+from source.config import config
 
 __plugin_name__ = 'help'
 __plugin_usage__ = '用法： 对我说 "help"，我会回复机器人功能表'
@@ -9,7 +10,12 @@ __plugin_usage__ = '用法： 对我说 "help"，我会回复机器人功能表'
 @on_command('help', aliases='帮助')
 async def _(session: CommandSession):
     localtime = time.asctime(time.localtime(time.time()))
-    await session.send('北京时间:' + localtime + " 指令索引\n"
+    command = ""
+    for i in config.start_with:
+        command += "'" + i + "'，"
+    command = command[:-1]
+    await session.send('北京时间:' + localtime + "\n"
+                                             "指令索引 注：以下的'/'在执行命令时可由"+command+"代替\n"
                                              '指令1：/help 或 /帮助 ：查看全部指令\n'
                                              '指令2：/sentence 或 /一言 ：随机获取一条名句\n'
                                              '指令3：/setu 或 /涩图 ： 随机获取一张非R18的动漫图片\n'
